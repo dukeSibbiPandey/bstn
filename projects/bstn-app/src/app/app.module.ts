728feaf82fa5bf752ad075b7f558765d665f3a74
Himanshu Pandey <himanshu.p89@gmail.com>
2022-03-10 23:45:55 +0530

    angular components

--- projects/bstn-app/src/app/app.module.ts
+++ projects/bstn-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './+components/shared/header/header.component';
+import { FooterComponent } from './+components/shared/footer/footer.component';
+import { LayoutComponent } from './+components/layout/layout.component';
+import { LoaderComponent } from './+components/shared/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
