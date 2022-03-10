9bda17a20aadade02a4231a37c14df1a24ad1479
Himanshu Pandey <himanshu.p89@gmail.com>
2022-03-10 23:56:51 +0530

    lib

--- projects/components-lib/src/public-api.ts
+++ projects/components-lib/src/public-api.ts
@@ -1,7 +1,6 @@
 /*
  * Public API Surface of components-lib
  */
-
-export * from './lib/components-lib.service';
-export * from './lib/components-lib.component';
+// export * from './lib/components-lib.service';
+// export * from './lib/components-lib.component';
 export * from './lib/components-lib.module';
