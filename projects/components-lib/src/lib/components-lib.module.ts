291f37c009f92ed8ea403417bbe873534d20e2ee
Himanshu Pandey <himanshu.p89@gmail.com>
2022-03-11 20:51:47 +0530

    test lib

--- projects/components-lib/src/lib/components-lib.module.ts
+++ projects/components-lib/src/lib/components-lib.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BstnAtomsModule } from './bstn-atoms/bstn-atoms.module';
 import { ComponentsLibComponent } from './components-lib.component';
 import { LoaderComponent } from './bstn-loader/loader.component';
-import { InputRestrictionsDirective } from './bstnDirectives/input-restrictions.directive';
 @NgModule({
   declarations: [
     ComponentsLibComponent,
     LoaderComponent,
-    InputRestrictionsDirective
   ],
   imports: [
     BstnAtomsModule
